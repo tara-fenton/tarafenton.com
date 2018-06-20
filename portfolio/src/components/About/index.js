cbe6434bc641d15d1c1bd7d25bb39f337346b1c9
Tara Fenton <tarafenton@ool-ad02048c.dyn.optonline.net>
2018-06-20 19:22:47 -0400

    about page

--- portfolio/src/components/About/index.js
+++ portfolio/src/components/About/index.js
@@ -1,15 +1,13 @@
 import React, { Component } from "react";
 import "./index.css";
 
-// import headshot from "../../images/headshot.png";
-
 class About extends Component {
   render() {
     return (
       <div className="about">
-        {/* <div className="headshot">
-          <img src={headshot} className="thumb" alt="Tara Fenton headshot" />
-        </div> */}
+        <div className="headshot">
+          <img src="/images/headshot.png" className="thumb" alt="Tara Fenton headshot" />
+        </div>
         <div className="right">
           <p>
             Coming from a flash development background before GA I was able to
--- portfolio/src/components/NavBar/index.js
+++ portfolio/src/components/NavBar/index.js
@@ -7,10 +7,11 @@ class NavBar extends Component {
   render() {
     return (
       <div className="nav">
-        <Link to="/">Home</Link>
+        {/* <Link to="/">Home</Link> */}
         <Link to="/about">About</Link>
         <Link to="/projects">Projects</Link>
         <Link to="/contact">Contact</Link>
+        <a href="http://tarafenton.com/resume/Tara%20Fenton.pdf">Resume</a>
       </div>
     );
   }
