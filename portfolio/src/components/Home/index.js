74f673aa4045f9820b0e81ee3cde8c7370d619df
Tara Fenton <tarafenton@Taras-MBP-3.fios-router.home>
2018-06-26 15:58:27 -0400

    skillicons added to home

--- portfolio/src/components/Home/index.js
+++ portfolio/src/components/Home/index.js
@@ -1,12 +1,22 @@
-import React, { Component } from 'react';
-import '../../App.css';
+import React, { Component } from "react";
+import "./index.css";
+import data from "../../data/skills.json";
 
 class Home extends Component {
   render() {
+    const skills = data.map(skill => {
+      return (
+        <img
+          src={`images/skillicons/${skill.image}`}
+          alt="Gregor Cresnar Noun Project"
+          className="skillicons"
+          key={skill.display_id}
+        />
+      );
+    });
     return (
       <div className="Home">
-      hommememmem
-
+          {skills}
       </div>
     );
   }
