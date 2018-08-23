bfd0f5def0a4749cab03f6c84b1aa3097a1af20f
Tara Fenton <tarafenton@ool-ad0201c5.dyn.optonline.net>
2018-08-23 15:13:36 -0400

    nav bar mobile styles

--- portfolio/src/components/NavBar/index.js
+++ portfolio/src/components/NavBar/index.js
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import "./index.css";
 
 class NavBar extends Component {
+
   render() {
+
     return (
       <div className="nav-container">
 
         <div className="nav">
           {/* <span className="logo"><img src="/images/TF_logo.jpg" /></span> */}
           {/* <Link to="/">Home</Link> */}
-          <Link to="/about">About</Link>
-          <Link to="/projects">Projects</Link>
-          <Link to="/contact">Contact</Link>
+          <NavLink to="/about" activeClassName="active">About</NavLink>
+          <NavLink to="/projects" activeClassName="active">Projects</NavLink>
+          <NavLink to="/contact" activeClassName="active">Contact</NavLink>
           <a
             href="http://tarafenton.com/resume/Tara%20Fenton.pdf"
             target="_blank"
